8f88197aacd8a5621473fb03f2f4d5b6bb1a06c4
“linhPham98” <linhthusinh98@gmail>
2024-01-10 00:13:41 +0900

    commit useContext

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
-import "./App.css";
 import { RouterProvider } from "react-router-dom";
 import router from "./routes/routes";
+import { createContext } from "react";
+import './App.scss';
+
+export const Context = createContext();
 
 function App() {
+  const dulieucss = {background:'blue', height:'500px',width:'500px'};
   return (
     <>
-      <RouterProvider router={router} />
+      <Context.Provider value={{data:dulieucss}}>
+        <RouterProvider router={router} />
+      </Context.Provider>
     </>
   );
 }
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,18 @@
-import React from 'react'
+import React, { Fragment, useContext, useState } from "react";
+import { Context } from "../App";
 
 export default function Home() {
+  const context = useContext(Context);
+  const [currentColor, setCurrentColor] = useState(context.data);
+
+  const toggleColor = () => {
+    setCurrentColor((prevColor) => (prevColor === context.data ? '' : context.data));
+  };
+
   return (
-    <div>Day la trang Home</div>
-  )
+    <Fragment>
+      <div style={{ ...currentColor }}>Day la trang Home</div>
+      <button onClick={toggleColor}>Toggle Color</button>
+    </Fragment>
+  );
 }
